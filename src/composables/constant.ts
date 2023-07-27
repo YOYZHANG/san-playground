1b190b4aea5aebd2a794cf859c5f3fa768236ad4
= <=>
2023-07-27 20:15:36 +0800

    feat: add default js

--- src/composables/constant.ts
+++ src/composables/constant.ts
@@ -1,8 +1,15 @@
-export const defaultHTML = `
-<div class="container">
-  San PlayGround
-</div>
-`.trim()
+export const defaultJS = `class MyApp extends san.Component {
+  static template = \`
+    <div class="container">
+      <input value="{= name =}" placeholder="please input">
+      Hello {{name}}!
+    </div>
+  \`;
+}
+
+let myApp = new MyApp();
+myApp.attach(document.body);
+`
 
 export const defaultCss = '.container {color: red;}'
 
--- src/composables/url.ts
+++ src/composables/url.ts
@@ -1,17 +1,18 @@
 import { ref } from 'vue'
 import { watchThrottled } from '@vueuse/core'
-import { STORAGE_KEY, defaultCss, defaultHTML } from './constant'
+import { STORAGE_KEY, defaultCss, defaultJS } from './constant'
 
 const urlParams = new URLSearchParams(location.search || localStorage.getItem(STORAGE_KEY) || '')
 
-export const inputJS = ref(decodeURIComponent(urlParams.get('html') || '') || defaultHTML)
+export const inputJS = ref(decodeURIComponent(urlParams.get('js') || '') || defaultJS)
 export const css = ref(decodeURIComponent(urlParams.get('css') || '') || defaultCss)
 
+console.log(inputJS.value, 'inputJS')
+
 watchThrottled([inputJS, css], () => {
-  console.log('inputJS change')
   const url = new URL('/', location.origin)
 
-  url.searchParams.set('html', encodeURIComponent(inputJS.value))
+  url.searchParams.set('js', encodeURIComponent(inputJS.value))
   url.searchParams.set('css', encodeURIComponent(css.value))
 
   localStorage.setItem(STORAGE_KEY, url.search)
